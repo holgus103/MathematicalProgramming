6b80ec51474414eb62cd37c3d4c5c40f59e63771
holgus103 <kubasuchan@hotmail.com>
2017-11-02 22:17:43 +0100

    Simplex more or less for LE

---
+++ lab2/Simplex.m
@@ -0,0 +1,62 @@
+function [x,flag] = Simplex(f, A, b)
+    n=length(f);
+    m=length(b);
+    n = n + m;
+    % extend A
+    A = [A eye(m)];
+    % allocate z
+    z = zeros(1, n);
+    % assign c
+    c = [f zeros(1,m)];
+    % calculate z - c
+    zmc = z-c;
+    % assign value vector
+    bf = b;
+    % assign base values
+    base = zeros(m, 1);
+    % get base indexes
+    baseIndexes = (m + 1) : n;   
+    it = 1;
+    while 1
+        % check for success
+        if(all(zmc > 0))
+            x = bf;
+            flag = 1;
+            return;
+        else
+            % check if solution exists
+            for i = 1 : n
+                % for every z-c < 0
+                if(zmc(i) < 0)
+                    if(all(A(:,i)<= 0))
+                        % contradiciton
+                        flag = -1;
+                        return;
+                    end
+                end
+            end
+            % no contradiction
+            % get minimum value
+            [valk, k] = min(zmc);
+            % find row 
+            [valr, r] = min(bf ./ transpose(A(:, k)));
+            eg = A(r, k);
+            % divide row by main element
+            A(r, :) = A(r,:) ./ eg;
+            bf(r) = bf(r)/eg;
+            for i = [1:(r-1), (r+1):m]
+                el = A(i, k);
+                A(i, :) = A(i,:) - el * A(r, :);
+                bf(i) = b(i) - el * bf(r);
+            end
+            baseIndexes(r) = k;
+            base(r) = c(k);
+            for i = 1:n
+                z(i) = dot(base, A(:, i));
+            end
+            zmc = z - c;
+ 
+        end
+    end
+end
+
