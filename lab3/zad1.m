0f53a2f4ce4e3cd094728a266cd4b941d1f6e667
holgus103 <kubasuchan@hotmail.com>
2017-12-20 23:37:35 +0100

    Files moved

--- lab3/zad1.m
+++ lab3/zad1.m
@@ -1,12 +1,16 @@
-A = [-3 5 -1 -7 7 -8 -5; 
-    -5 -3 -8 -6 -8 0 0;
-    8 8 -2 8 -6 4 -8;
-    -7 5 -8 -3 -6 5 -1;
-    4 -1 6 1 5 -3 1];
+f = [8;-1;6;0];
+A = [-6,-6,-3,3; -2, 6, 5, 0; 4, 4, -6, -4];
+b = [-8; -7; -3];
+lb = [0;0;0;0];
+ub = [];
+options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'iter');
+[x, fval, exitflag, output, lambda] = linprog(f, A, b, [], [], lb, ub, options);
 
-b = [1 7 8 1 1];
-c = [-4 -5 0 4 -5 3 -5];
+f = [-1;-8;-3;5; -3];
+A = [-3,-2,8,-3,8; 7, 7, 1, -3, -6; 7, -2, 6, -1, 3; 0, 4, 5, 0, -3];
+b = [-18; -20; -18; -14];
+lb = [0;0;0;0];
+ub = [];
+options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'iter');
+[x2, fval2, exitflag, output, lambda] = linprog(f, A, b, [], [], lb, ub, options);
 
-A2 = transpose(A);
-c2 = b;
-b2 = c;
