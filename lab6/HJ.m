0e93bea0a66acca16d6dec188270fb61311bd562
holgus103 <kubasuchan@hotmail.com>
2017-12-22 02:20:50 +0100

    Hooke algorithm

--- lab6/HJ.m
+++ lab6/HJ.m
@@ -1,70 +1,43 @@
-function xb = HJ(f, x0, e, delta, alfa, beta, maxIter)
-    function [xr, oldx0, fail] = trialStage(x0, maxIter)
-        fail = 0;
-        oldx0 = x0;
-        k = 0;
-        while 1
-            xr = x0;
-            fx = f(x0);
-            for i = 1:n
-                xn = xr + delta * vec(i, 1:n);
-                fn = f(xn);
-                if(fx > fn)
-                    oldx0 = xr;
-                    xr = xn;
-                    fx = fn;
-                else
-                    xn = xr - delta  * vec(i, 1:n);
-                    fn = f(xn);
-                    if(fx > fn)
-                        oldx0 = xr;
-                        xr = xn;
-                        fx = fn;
-                    else
-                        if(k == maxIter)
-                            fail = 1;
-                            return;
-                        end
-                        delta = beta * delta;
-                        if(delta < e)
-                            fail = 1;
-                            return;
-                        end
-                        break;
-                    end
-                end
-            end
-            if(i == n)
-                return;
-            end
-            k = k + 1;
-        end
-    end
-    
-    xb = x0;
+function x = HJ(f, x0, e, delta, alpha, beta)    
+    x = x0;
     oldx = x0;
     n = size(x0, 2);
     vec = eye(n);
-    while 1
-
-        [xb, oldx, fail] = trialStage(xb, maxIter);
-        if(fail == 1)
-            return;
-        end
-        
-         x = xb + alfa*(xb - oldx);
-        
-        [xt, a, fail] = trialStage(x, 1);
-        if(fail == 0)
-            xb = xt;
-            oldx = a;
-        else
-            delta = delta * beta;   
-            if(delta < e)
-                return;
+    while delta > e
+        while delta > e
+            fx = f(x);
+            for i = 1:n
+                xn = x + delta * vec(i, 1:n);
+                fn = f(xn);
+                if(fx > fn)
+                    x = xn;
+                    fx = fn;
+                else
+                    x = x - delta  * vec(i, 1:n);
+                    fn = f(xn);
+                    if(fx > fn)
+                        x = xn;
+                        fx = fn;
+                    end
+                end
+            end
+            if(oldx == x)
+                delta = delta * beta;
+            else
+                if f(x)>=f(oldx)
+                    x = oldx;
+                    delta = delta*beta;
+                else
+                    break;
+                end
             end
         end
+        oldoldx = oldx;
+        oldx = x;
+        x = alpha*(oldx - oldoldx);
+   end
             
-    end
 end
+
+    
 
